259d3252bd9f0b27bc70c880ee041dd4b81cca13
Eddie Jorden <eddiejorden.tech@gmail.com>
2021-01-22 10:37:05 -0800

    cleaned up a little

--- src/components/reasons.js
+++ src/components/reasons.js
@@ -1,7 +1,5 @@
 export const reasons = [
-    "you dont even smoke weed",
-    "you cant even drive a manual",
-    "do you even lift bro?",
+	'you dont even smoke weed',
+	'you cant even drive a manual',
+	'do you even lift bro?',
 ];
-
-export const test = "hello?"
